d7c7343ab47fdddd6054fddf49b5ec786bb3a300
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-09-25 15:16:47 -0500

    Reworked js

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,91 @@
 // Assignment Code
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-var generatePassword = function(){
-
-}
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var pwd = [];
+var charLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var charUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var number = [0,1,2,3,4,5,6,7,8,9];
+var sym = ['!', '@', '#', '%', '$'];
+console.log(number);
+console.log(typeof lower);
+
+var numberChoice = false;
+var upperChoice = false;
+var lowerChoice = false;
+var symbolChoice = false;
+
+var password = [];
+
+function generatePassword() {
+
+  var length = ""
+    while (isNaN(length) || length < 8 || length > 128) {
+      length = prompt ("Choose a number between 8-128 for the length of your password.");
+      if (length === null){
+        break ;
+      }
+    }
+
+numberChoice = confirm("Do you want your password to contain NUMBERS?");
+
+upperChoice = confirm("Do you want your password to contain UPPERCASE LETTERS?");
+
+lowerChoice = confirm( "Do you want your password to contain LOWERCASE LETTERS?");
+
+symbolChoice = confirm("Do you want your password to contain SPECIAL CHARACTERS/SYMBOLS?");
+
+  if (numberChoice === false && upperChoice === false && lowerChoice === false && symbolChoice === false) {
+    alert("At least one of the criteria's must be selected. Please start over.")};
+    
+  var lengthValue = length * 1;
+  console.log(typeof lengthValue);
+
+  var charAvail = [];
+  
+    if (numberChoice === true) {
+      charAvail.concat(number);
+      charAvail =  charAvail.concat(number);
+    }
+
+    if (upperChoice === true) {
+      charAvail.concat(upper);
+      charAvail =  charAvail.concat(upper);
+    }
+
+    if (lowerChoice === true) {
+      charAvail.concat(lower);
+      charAvail =  charAvail.concat(lower);
+    }
+
+    if(symbolChoice === true) {
+      charAvail.concat(lower);
+      charAvail =  charAvail.concat(symbol);
+    }
+
+
+  var password = [];
+  function passChar(lengthValue, charAvail) {
+
+    for (var i = 0; i < lengthValue; i++){
+    password.push(charAvail[Math.floor(Math.random() * charAvail.length)]);
+  }
+  }
+  
+  passChar(lengthValue, charAvail);
+
+  console.log(password);
+
+  password = password.join('');
+
+return password;
+
+}
+
